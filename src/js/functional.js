4969c9d5f71a9f77911a6723d03769abbaa580cf
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-05 18:18:32 -0500

    added new js

---
+++ src/js/functional.js
@@ -0,0 +1,18 @@
+document.on("load", () => {
+    let request = new XMLHttpRequest();
+    const btn = document.getElementById("apiButton");
+    
+    btn.addEventListener("click", (e) => {
+        
+        e.preventDefault();
+        request.addEventListener("load", function() {
+            const apiDataContainer = document.getElementById("apiDataContainer");
+            let textNode = document.createTextNode(this.responseText);
+            let pargraph = document.createElement("p").appendChild(textNode);
+            apiDataContainer.appendChild(pargraph);
+        });
+        request.open("GET", "http://localhost:8080/api/1.0/twitter/timeline");
+        request.send();
+        
+    });
+});
