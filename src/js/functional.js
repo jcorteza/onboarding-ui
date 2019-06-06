acd742a693592b5600e9ba73f7371688129432ee
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-06 11:24:21 -0500

    added error handler

--- src/js/functional.js
+++ src/js/functional.js
@@ -5,14 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
     btn.addEventListener("click", (e) => {
         
         e.preventDefault();
+        const apiDataContainer = document.getElementById("apiDataContainer");
         request.addEventListener("load", function() {
-            const apiDataContainer = document.getElementById("apiDataContainer");
             let textNode = document.createTextNode(this.responseText);
-            let pargraph = document.createElement("p").appendChild(textNode);
-            apiDataContainer.appendChild(pargraph);
+            appendParagraph(textNode, apiDataContainer);
         });
+        request.addEventListener("error", () => {
+            let textNode = document.createTextNode("This content is not currently available. Please try again later.");
+            appendParagraph(textNode, apiDataContainer);
+        })
         request.open("GET", "http://localhost:8080/api/1.0/twitter/timeline");
         request.send();
         
     });
 });
+
+const appendParagraph = (textNode, container) => {
+    let pargraph = document.createElement("p").appendChild(textNode);
+    container.appendChild(pargraph);
+}
