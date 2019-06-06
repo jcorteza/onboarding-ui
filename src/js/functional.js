194e28f25bc912538aedcfab98a92d242325500f
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-06 12:20:35 -0500

    debugged

--- src/js/functional.js
+++ src/js/functional.js
@@ -1,26 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
     let request = new XMLHttpRequest();
     const btn = document.getElementById("apiButton");
+    const apiDataContainer = document.getElementById("apiDataContainer");
+    const appendParagraph = (textNode, container) => {
+        let pargraph = document.createElement("p").appendChild(textNode);
+        container.append(pargraph);
+    }
     
+    request.addEventListener("load", function() {
+        let textNode = document.createTextNode(this.responseText);
+        appendParagraph(textNode, apiDataContainer);
+    });
+
+    request.addEventListener("error", () => {
+        let textNode = document.createTextNode("This content is not currently available. Please try again later.");
+        appendParagraph(textNode, apiDataContainer);
+    });
+
     btn.addEventListener("click", (e) => {
         
         e.preventDefault();
-        const apiDataContainer = document.getElementById("apiDataContainer");
-        request.addEventListener("load", function() {
-            let textNode = document.createTextNode(this.responseText);
-            appendParagraph(textNode, apiDataContainer);
-        });
-        request.addEventListener("error", () => {
-            let textNode = document.createTextNode("This content is not currently available. Please try again later.");
-            appendParagraph(textNode, apiDataContainer);
-        })
+
+        apiDataContainer.innerHTML = "";
         request.open("GET", "http://localhost:8080/api/1.0/twitter/timeline");
         request.send();
         
     });
 });
-
-const appendParagraph = (textNode, container) => {
-    let pargraph = document.createElement("p").appendChild(textNode);
-    container.appendChild(pargraph);
-}
