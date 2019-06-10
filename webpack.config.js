793e5fda5b1cfb67e07263439c65aa3eb5f3de1f
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-10 11:28:20 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,30 @@
 const path = require("path");
 
 module.exports = {
+    mode: "production",
+    entry: "./src/main.js",
+    devtool: "cheap-module-source-map",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    "file-loader"
+                ]
+            }
+        ]
+    },
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         compress: true,
