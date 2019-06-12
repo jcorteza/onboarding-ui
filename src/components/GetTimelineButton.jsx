d17d30ae13d5f32941c778f62b2ed5ed7016f7f1
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-11 19:16:40 -0500

    created components

---
+++ src/components/GetTimelineButton.jsx
@@ -0,0 +1,7 @@
+class GetTimelineButton extends React.Component {
+    render() {
+        return <button id="apiButton" type="button" >Get Twitter Timeline</button>;
+    }
+}
+
+export default GetTimelineButton;
