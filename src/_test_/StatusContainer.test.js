8e010890bc5b90c72582abb83560a3db34defd51
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-14 13:29:02 -0500

    modified status container test

--- src/_test_/StatusContainer.test.js
+++ src/_test_/StatusContainer.test.js
@@ -2,7 +2,8 @@ import {shallow} from "enzyme";
 import StatusContainer from "../components/StatusContainer.jsx";
 
 test("", () => {
-    const statusContainer = shallow(<StatusContainer />);
+    const mockGetFormattedDate = jest.fn(() => "June 13"); 
+    const statusContainer = shallow(<StatusContainer date={new Date()} postUrl="http://www.weblink.com" message="test message"/>);
 
     expect(statusContainer).toHaveReturn("<div className=\"textDiv\"></div>");
 });
