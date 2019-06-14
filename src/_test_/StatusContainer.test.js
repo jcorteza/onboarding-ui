ac3ac5aa828789425ddc7a3bb535454313b65780
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-14 14:17:38 -0500

    updated expect statements

--- src/_test_/StatusContainer.test.js
+++ src/_test_/StatusContainer.test.js
@@ -5,7 +5,7 @@ test("", () => {
     const mockGetFormattedDate = jest.fn(() => "June 13"); 
     const statusContainer = shallow(<StatusContainer date={new Date()} postUrl="http://www.weblink.com" message="test message"/>);
 
-    expect(statusContainer).toHaveReturnedWith(
+    expect(statusContainer.render).toHaveReturnedWith(
         <div className="textDiv">
             <p>June 13</p>
             <a href="http://www.weblink.com" target="_blank">
--- src/_test_/TweetContainer.test.js
+++ src/_test_/TweetContainer.test.js
@@ -2,7 +2,24 @@ import {shallow} from "enzyme";
 import TweetContainer from "../components/TweetContainer.jsx";
 
 test("", () => {
-    const tweetContainer = shallow(<TweetContainer />);
-
-    expect(tweetContainer).toHaveReturn("<div className=\"tweetContainer\"></div>");
+    let testDate = new Date();
+    let tweetContainer = shallow(<TweetContainer postUrl="www.post.com" message="test message" date={testDate}/>);
+    
+    expect(tweetContainer.props.user).toBeFalsy();
+    expect(tweetContainer.render).toHaveReturnedWith(
+        <div className="tweetContainer">
+            <UserContainer />
+            <StatusContainer postUrl="www.post.com" message="test message" date={testDate}/>
+        </div>
+    );
+    
+    tweetContainer = shallow(<TweetContainer postUrl="www.post.com" message="test message" date={testDate} user={{name: "Twitter User", twHanlde: "twitterUser", profileImgUrl: "www.profilePic.com"}}/>);
+    
+    expect(tweetContainer.props.user).toBeTruthy();
+    expect(tweetContainer.render).toHaveReturnedWith(
+        <div className="tweetContainer">
+            <UserContainer userName="Twitter User" userHandle="twitterUser" profileImgUrl="www.profilePic.com"/> :
+            <StatusContainer postUrl="www.post.com" message="test message" date={testDate}/>
+        </div>
+    );
 });
