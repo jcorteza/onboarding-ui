cd346dac77d579adf94629c4a5ec4109f82b1970
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-14 11:19:58 -0500

    modified button test

--- src/_test_/GetTimelineButton.test.js
+++ src/_test_/GetTimelineButton.test.js
@@ -2,7 +2,11 @@ import {shallow} from "enzyme";
 import GetTimelineButton from "../components/GetTimelineButton.jsx";
 
 test("", () => {
-    const button = shallow(<GetTimelineButton />);
+    const mockSendRequest = jest.fn();
+    const button = shallow(<GetTimelineButton onClick={mockSendRequest}/>);
+
+    fireEvent.click(button);
+    expect(mockSendRequest).toHaveBeenCalledTimes(1);
 
     expect(button).toHaveReturn("<button>Get Home Timeline</button>");
 });
