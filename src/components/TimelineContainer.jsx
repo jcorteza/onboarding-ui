08cf47a95d1419ed9a449417bc4d7f67ae5f807e
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-17 11:40:19 -0500

    updated conditional statements

--- src/components/TimelineContainer.jsx
+++ src/components/TimelineContainer.jsx
@@ -6,38 +6,50 @@ class TimelineContainer extends React.Component {
         super(props);
 
         this.state = {
-            readyState: 0
+            data: [],
+            errorOccurred: false
+        }
+
+        this.updateStatus = (responseStatus, responseData) => {
+            if(responseStatus === 200) {
+                this.setState({
+                    data: JSON.parse(responseData),
+                    errorOccurred: false
+                });
+            } else if (responseStatus !== 200) {
+                this.setState({
+                    data: [],
+                    errorOccurred: true
+                });
+            }
         }
     }
 
     componentDidMount() {
-        requestHandler.initRequest(requestHandler.request, () => {
-            this.setState({readyState: requestHandler.request.readyState});
-        });
-        requestHandler.sendRequest(requestHandler.request);
+        requestHandler(this.updateStatus);
     }
 
     render() {
-        let timelineContainer = <div id="timelineContainer"></div>;
-        if (this.state.readyState < 4) {
-            timelineContainer = (
-                <div id="timelineContainer">
-                    <p>Loading your Twitter timeline...</p>
-                </div> 
-            ); 
-        } else if(this.state.readyState  === 4 && requestHandler.request.status !== 200) {
+        let timelineContainer;
+        if(this.state.errorOccurred === true) {
             timelineContainer = (
                 <div id="timelineContainer">
                     <p>This content is not currently available. Please try again later.</p>
                 </div>
             );
-        } else if(this.state.readyState  === 4 && requestHandler.request.status === 200) {
+        } else if(this.state.errorOccurred === false && this.state.data.length > 0) {
             timelineContainer = (
                 <div id="timelineContainer">
-                    {JSON.parse(requestHandler.request.response).map(status => 
+                    {this.state.data.map(status => 
                         <TweetContainer key={status.postUrl} user={status.user} postUrl={status.postUrl} message={status.message} createdAt={status.createdAt}/>
                     )}
                 </div> 
+            );
+        } else if (this.state.errorOccurred === false) {
+            timelineContainer = (
+                <div id="timelineContainer">
+                    <p>Loading your Twitter timeline...</p>
+                </div>
             );
         }
 
@@ -46,7 +58,11 @@ class TimelineContainer extends React.Component {
                 <button id="apiButton" type="button" onClick={
                     (e) => {
                         e.preventDefault();
-                        requestHandler.sendRequest(requestHandler.request);
+                        this.setState({
+                            data: [],
+                            errorOccurred: false
+                        })
+                        requestHandler(this.updateStatus);
                     }
                 }>Get Twitter Timeline</button>
                 {timelineContainer}
--- src/js/requestHandler.js
+++ src/js/requestHandler.js
@@ -1,15 +1,13 @@
-const requestHandler = {
-
-    request: new XMLHttpRequest(),
-    initRequest: (request, callback) => {
-        request.onreadystatechange = () => {
-            callback();
-        };
-    },
-    sendRequest: (request)=> {
-        request.open("GET", "http://localhost:8080/api/1.0/twitter/timeline");
-        request.send();
-    }
+function requestHandler(callback) {
+    const request = new XMLHttpRequest();
+    request.addEventListener("error", () => {
+        callback(request.status, request.response);
+    });
+    request.addEventListener("load", () => {
+        callback(request.status, request.response);
+    });
+    request.open("GET", "http://localhost:8080/api/1.0/twitter/timeline");
+    request.send();
 }
 
 export default requestHandler;
