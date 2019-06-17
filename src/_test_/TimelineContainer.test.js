e4e2c086eb40bf3a1f6e0c946ea00ff292965013
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-17 15:19:40 -0500

    updated testing logic

--- src/_test_/TimelineContainer.test.js
+++ src/_test_/TimelineContainer.test.js
@@ -1,13 +1,47 @@
 import {shallow} from "enzyme";
 import TimelineContainer from "../components/TimelineContainer.jsx";
+import handleRequest from "../js/handleRequest.js";
+
+jest.mock("../js/handleRequest.js");
 
 test("timelineContainer updates based on state", () => {
-    const timelineContainer = shallow(<TimelineContainer error="false" finished="false" response="[]"/>);
+    const timelineContainer = shallow(<TimelineContainer />);
+    const errorRendering = (
+        <div id="apiContainer">
+            <button onClick={timelineContainer.}>Get Home Timeline</button>
+            <div className="timelineContainer">
+                <p>Content Not Available at this Time</p>
+            </div>
+        </div>
+    );
+    
+    handleRequest.mockImplementation((callback) => {
+        callback("");
+    });
 
-    component
+    expect(timelineContainer.render).toHaveReturnedWith(
+        <div id="apiContainer">
+            <button onClick={timelineContainer.}>Get Home Timeline</button>
+            <div className="timelineContainer">
+                <p>Loading Twitter Timeline...</p>
+            </div>
+        </div>
+    );
+    expect(timelineContainer.componentDidRender).toHaveBeenCalled();
+    expect(handleRequest).toHaveBeenCalled(); 
+    expect(timelineContainer.handleClick).toHaveBeenCalledWith("");
+    expect(timelineContainer.setState).toHaveBeenCalledWith({
+        data: [],
+        errorOccurred: true
+    });
+    expect(timelineContainer.render).toHaveReturnedWith(errorRendering);
+    
+    
+    timelineContainer
         .find("button")
         .simulate("click");
-
-    expect(fn).toHaveBeenCalled(); 
-    expect(timelineContainer).toHaveReturn("<div className=\"timelineContainer\"></div>");
+    expect(handleRequest).toHaveBeenCalled(); 
+    expect(timelineContainer.handleClick).toHaveBeenCalled();
+    expect(timelineContainer.render).toHaveReturnedWith(errorRendering);
+    
 });
