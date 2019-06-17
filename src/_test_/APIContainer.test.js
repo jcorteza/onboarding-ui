e67c9b9eda266b1113085ef31e47fd6921b829fd
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-17 15:24:53 -0500

    removed unnecessary tests

--- src/_test_/APIContainer.test.js
+++
@@ -1,8 +0,0 @@
-import {shallow} from "enzyme";
-import APIContainer from "../components/APIContainer.jsx";
-
-test("apiContainer returns button and div", () => {
-    const apiContainer = shallow(<APIContainer />);
-
-    expect(apiContainer).toHaveReturn("<div id=\"apiContainer\"></div>");
-});
