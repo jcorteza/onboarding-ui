2ca3bdb31cf20b2b1d4dbe16cd0817070bfb9750
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-17 13:50:08 -0500

    moved import statements to top:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import "./style/css/main.css";
+import TimelineContainer from "./components/TimelineContainer.jsx";
+
 document.addEventListener("DOMContentLoaded", () => {
-    import "./style/css/main.css";
-    import TimelineContainer from "./components/TimelineContainer.jsx";
     document.getElementById("root");
-    
     ReactDOM.render(<TimelineContainer />, document.getElementById("root"));
 });
