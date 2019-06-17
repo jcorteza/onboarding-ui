513b95fc12e1ca88bf326781c1665a99df86214f
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-17 15:36:30 -0500

    changed mock function call

--- src/_test_/StatusContainer.test.js
+++ src/_test_/StatusContainer.test.js
@@ -3,11 +3,11 @@ import StatusContainer from "../components/StatusContainer.jsx";
 
 test("statusContainer renders with data from props", () => {
     const mockGetFormattedDate = jest.fn(() => "June 13"); 
-    const statusContainer = shallow(<StatusContainer date={new Date()} postUrl="http://www.weblink.com" message="test message"/>);
+    const statusContainer = shallow(<StatusContainer postUrl="http://www.weblink.com" message="test message"/>);
 
     expect(statusContainer.render).toHaveReturnedWith(
         <div className="textDiv">
-            <p>June 13</p>
+            <p>{mockGetFormattedDate()}</p>
             <a href="http://www.weblink.com" target="_blank">
                 <p>test message</p>
             </a>
