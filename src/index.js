d649837327fd0e95c147abfcca8af037c2cdd37f
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-17 13:42:04 -0500

    added document event listener

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import "./style/css/main.css";
-import TimelineContainer from "./components/TimelineContainer.jsx";
-document.getElementById("root");
-
-ReactDOM.render(<TimelineContainer />, document.getElementById("root"));
+document.addEventListener("DOMContentLoaded", () => {
+    import "./style/css/main.css";
+    import TimelineContainer from "./components/TimelineContainer.jsx";
+    document.getElementById("root");
+    
+    ReactDOM.render(<TimelineContainer />, document.getElementById("root"));
+});
