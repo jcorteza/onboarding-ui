1f32a6d2ed3a68e9cfb02d82903394e9b7e7f191
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-17 15:41:24 -0500

    added constructor expected statement

--- src/_test_/StatusContainer.test.js
+++ src/_test_/StatusContainer.test.js
@@ -5,6 +5,7 @@ test("statusContainer renders with data from props", () => {
     const mockGetFormattedDate = jest.fn(() => "June 13"); 
     const statusContainer = shallow(<StatusContainer postUrl="http://www.weblink.com" message="test message"/>);
 
+    expect(StatusContainer).toHaveBeenCalled();
     expect(statusContainer.render).toHaveReturnedWith(
         <div className="textDiv">
             <p>{mockGetFormattedDate()}</p>
