300c294e4af58774fccbb279b23bb12612515066
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-17 15:52:39 -0500

    updated testing logic

--- src/_test_/TimelineContainer.test.js
+++ src/_test_/TimelineContainer.test.js
@@ -1,47 +1,49 @@
 import {shallow} from "enzyme";
 import TimelineContainer from "../components/TimelineContainer.jsx";
-import handleRequest from "../js/handleRequest.js";
+import requestHandler from "../js/requestHandler.js";
 
-jest.mock("../js/handleRequest.js");
+jest.mock("../js/requestHandler.js");
 
 test("timelineContainer updates based on state", () => {
     const timelineContainer = shallow(<TimelineContainer />);
     const errorRendering = (
         <div id="apiContainer">
-            <button onClick={timelineContainer.}>Get Home Timeline</button>
+            <button id="apiButton" type="button" onClick={timelineContainer.handleClick}>Get Home Timeline</button>
             <div className="timelineContainer">
-                <p>Content Not Available at this Time</p>
+                <p>This content is not currently available. Please try again later.</p>
             </div>
         </div>
     );
     
-    handleRequest.mockImplementation((callback) => {
+    requestHandler.mockImplementation((callback) => {
         callback("");
     });
 
+    expect(TimelineContainer).toHaveBeenCalled();
     expect(timelineContainer.render).toHaveReturnedWith(
         <div id="apiContainer">
-            <button onClick={timelineContainer.}>Get Home Timeline</button>
+            <button id="apiButton" type="button" onClick={timelineContainer.handleClick}>Get Home Timeline</button>
             <div className="timelineContainer">
-                <p>Loading Twitter Timeline...</p>
+                <p>Loading your Twitter timeline...</p>
             </div>
         </div>
     );
-    expect(timelineContainer.componentDidRender).toHaveBeenCalled();
-    expect(handleRequest).toHaveBeenCalled(); 
-    expect(timelineContainer.handleClick).toHaveBeenCalledWith("");
+    expect(timelineContainer.componentDidMount).toHaveBeenCalled();
+    expect(requestHandler).toHaveBeenCalledWith(timelineContainer.updateStatus); 
+    expect(timelineContainer.updateStatus).toHaveBeenCalledWith("");
     expect(timelineContainer.setState).toHaveBeenCalledWith({
         data: [],
         errorOccurred: true
     });
     expect(timelineContainer.render).toHaveReturnedWith(errorRendering);
     
-    
     timelineContainer
-        .find("button")
+        .find("button#apiButton")
         .simulate("click");
-    expect(handleRequest).toHaveBeenCalled(); 
     expect(timelineContainer.handleClick).toHaveBeenCalled();
+    expect(timelineContainer.setState).toHaveBeenCalled();
+    expect(requestHandler).toHaveBeenCalledWith(timelineContainer.updateStatus); 
+    expect(timelineContainer.updateStatus).toHaveBeenCalledWith("");
     expect(timelineContainer.render).toHaveReturnedWith(errorRendering);
     
 });
