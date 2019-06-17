a17bac59ee3b263ebea84e5ef74fa8ca54f116c1
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-17 10:26:43 -0500

    deleted older version of requestHandler

--- src/js/sendRequest.js
+++
@@ -1,8 +0,0 @@
-const sendRequest = (request, callback) => {
-    request.onload(callback());
-    request.onerror(callback());
-    request.open("GET", "http://localhost:8080/api/1.0/twitter/timeline");
-    request.send();
-}
-
-export default sendRequest;
