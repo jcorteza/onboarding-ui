8e9c21920d179ed8bc1971f70b0bd1b8bfd6db31
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-07-01 17:52:31 -0500

    created post tweet service

---
+++ src/service/postTweetToTimeline.js
@@ -0,0 +1,26 @@
+const postTweetToTimeline = (tweetText) => {
+    let data = {message: tweetText};
+
+    return new Promise((resolve, reject) => {
+        fetch("http://localhost:8080/api/1.0/twitter/tweet", {
+            method: "POST",
+            body: JSON.stringify(data)
+        }).then((fetchResponse) => {
+
+            if(fetchResponse.status === 200) {
+
+                resolve({successful: true})
+
+            } else {
+
+                resolve({successful: false});
+
+            }
+
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+}
+
+export default postTweetToTimeline;
